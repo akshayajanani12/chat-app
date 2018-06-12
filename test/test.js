919d3ccc40f74c71dfb9132d5744dcf5dd2fa9ca
akshayajanani12 <36192918+akshayajanani12@users.noreply.github.com>
2018-06-12 14:30:20 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,15 +1,16 @@
 import { Selector } from 'testcafe'; // first import testcafe selectors
 
 fixture `Getting Started`// declare the fixture
-    .page `https://devexpress.github.io/testcafe/example`;  // specify the start page
+    .page `http://localhost:3001`;  // specify the start page
 
 
 //then create a test and place your code there
 test('My first test', async t => {
     await t
-        .typeText('#developer-name', 'John Smith')
-        .click('#submit-button')
+        .typeText('#name', 'John Smith')
+        .typeText('#room', '1')
+        .click('#submit')
 
         // Use the assertion to check if the actual header text is equal to the expected one
-        .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!');
+        // .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!');
 });
