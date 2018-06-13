52a5c05ca683997b90e0ba36829c527f5a76899c
akshayajanani12 <36192918+akshayajanani12@users.noreply.github.com>
2018-06-13 13:06:11 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,16 +1,18 @@
-import { Selector } from 'testcafe'; // first import testcafe selectors
+import {
+    Selector
+} from 'testcafe'; // first import testcafe selectors
 
-fixture `Getting Started`// declare the fixture
-    .page `http://localhost:3001`;  // specify the start page
+fixture Getting Started // declare the fixture
+    .page http://localhost:3000; // specify the start page
 
 
 //then create a test and place your code there
-test('My first test', async t => {
+test('Login', async t => {
     await t
-        .typeText('#name', 'John Smith')
-        .typeText('#room', '1')
-        .click('#submit')
+        .typeText("[name='name']", "Chief Bogo")
+        .typeText("[name='room']", "1")
+        .click('#join')
 
-        // Use the assertion to check if the actual header text is equal to the expected one
-        // .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!');
+    // Use the assertion to check if the actual header text is equal to the expected one
+    // .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!');
 });
